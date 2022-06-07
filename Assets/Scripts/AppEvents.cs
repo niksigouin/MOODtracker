80217724a5293bdc75ba3bef6a628aa8892ec620
Anne-Julie <anne-julie.tremblay@pixsenses.com>
2022-06-06 21:48:58 -0400

    AppEvent script update

---
+++ Assets/Scripts/AppEvents.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppEvents : MonoBehaviour
+{
+    public static AppEvents Instance;
+
+    public Action<DateTime> DateSelectionChanged;
+    public Action<DateTime, int, int> NewLogEntry;
+    public Action<DateTime, string, Color> NewCategoryCreated;
+    
+    private void Start()
+    {
+        if (Instance != null && Instance != this) Destroy(this.gameObject);
+        else Instance = this;
+    }
+}
